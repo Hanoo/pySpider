8e61d19acd9f2750e1ff43788362bfd32a28ecb7
Cyanus <yanhan09@163.com>
2024-11-07 00:29:15 +0800

    完善100Bucks图片下载代码

--- pic_spider/test.py
+++ pic_spider/test.py
@@ -1,11 +1,27 @@
+import sys
+
 import requests
 import os
 
 
+def create_folder(force):
+    folder_path = f'{base_folder}{seperator}{model_name}{seperator}{title}'
+    if os.path.exists(folder_path) and not force:
+        print('文件系统中存在同名目录，请重新设置')
+        sys.exit(-1)
+    elif os.path.exists(folder_path) and force:
+        os.removedirs(folder_path)
+        print('移除已存在的目录')
+    os.makedirs(folder_path)
+    print('创建目录成功')
+    return folder_path
+
+
 # 下载url中的内容并保存
-def download_image_via_proxy():
+def download_image_via_proxy(url, folder):
     # 提取文件名
     file_name = os.path.basename(url)
+    full_path = f'{folder}{seperator}{file_name}'
 
     try:
         # 通过代理请求URL
@@ -13,7 +29,7 @@ def download_image_via_proxy():
         response.raise_for_status()  # 检查请求是否成功
 
         # 将图片内容写入文件
-        with open(file_name, 'wb') as file:
+        with open(full_path, 'wb') as file:
             for chunk in response.iter_content(1024):
                 file.write(chunk)
         print(f"图片已保存为 {file_name}")
@@ -33,7 +49,7 @@ def generate_urls(start_num, loop_count, char1, char2):
 
     result = []
     for i in range(loop_count):
-        num = (start_num + i) % 10  # 确保数字在0-9之间循环
+        num = (start_num + i)
         # 如果数字是个位数，补0
         num_str = f"{num:02}"
         result.append(f"{char1}{num_str}{char2}")
@@ -41,9 +57,7 @@ def generate_urls(start_num, loop_count, char1, char2):
     return result
 
 
-# 示例调用
-url = "https://www.popo8.com/gift/gift_314.png"  # 你要下载的图片URL
-proxy_host = "192.168.0.210"
+proxy_host = "127.0.0.1"
 proxy_port = "8001"
 
 # 设置代理
@@ -54,10 +68,18 @@ proxies = {
 
 # 示例调用
 start_num = 0
-loop_count = 12
-char1 = "https://www.popo8.com/gift/"
+loop_count = 16
+char1 = "https://content9.100bucksbabes.com/linseyworld.com/0039/"
 char2 = ".jpg"
 result = generate_urls(start_num, loop_count, char1, char2)
 print(result)
 
-# download_image_via_proxy()
+model_name = 'Linsey Dawn'
+title = 'Nude-Oil-2'
+seperator = os.sep
+
+base_folder = f'E:{seperator}Download'
+res = create_folder(True)
+
+for img_url in result:
+    download_image_via_proxy(img_url, res)
